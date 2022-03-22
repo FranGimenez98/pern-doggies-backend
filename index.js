6e859534c74b26aa306cfe9f3606558718f1ef40
franprogweb98 <franprogweb98@gmail.com>
2022-03-22 15:09:30 -0300

    cors fix

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ const { PORT } = process.env;
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const cors = require('cors');
+const app = require('express')
+
+app.use(cors())
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
